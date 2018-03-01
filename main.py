87ac2165d51da0d495c2b9f7689d783fb384534e
FrederickJ96 <fritzj96@gmail.com>
2018-03-01 00:33:40 -0800

    Starting to take shape, basic functionality now available

--- main.py
+++ main.py
@@ -2,10 +2,12 @@
 from alpha_vantage.timeseries import TimeSeries
 import datetime
 import json
-import openpyxl
+from openpyxl import Workbook
 from decimal import Decimal
+date = None
+workingRow = 2
 ts = TimeSeries('N1RDW0MA9WZPJGCK')
-introMessage = 'Welcome to StockCell;'
+introMessage = 'Welcome to StockCell,'
 tickerInput =  'Please input your desired ticker   > '
 tickerError = 'Not a valid ticker'
 dateInput = 'What date would you like data for? > '
@@ -41,20 +43,42 @@ def api_call(ticker):
             break
         except ValueError:
             print(errorMessage)
-def date_entry():
+def date_entry(): # TODO: Regex for date entry validation
     while True:
         try:
+            global date
             date = (input(dateInput))
             return date
         except ValueError:
             print(dateError)
 def data_reader(data, date):
-    open_value =  'Open:   ' + data[date]['1. open']
-    high_value =  'High:   ' + data[date]['2. high']
-    low_value =   'Low:    ' + data[date]['3. low']
-    close_value = 'Close:  ' + data[date]['4. close']
-    vol_value =   'Volume: ' + data[date]['5. volume']
+    open_value = data[date]['1. open']
+    high_value = data[date]['2. high']
+    low_value = data[date]['3. low']
+    close_value = data[date]['4. close']
+    vol_value = data[date]['5. volume']
     value_dict = (open_value, high_value, low_value, close_value, vol_value)
     return value_dict
+def rowfill(fivevalues):
+    global workingRow
+    ws['C'+ str(workingRow)] = fivevalues[0]
+    ws['D'+ str(workingRow)] = fivevalues[1]
+    ws['E'+ str(workingRow)] = fivevalues[2]
+    ws['F'+ str(workingRow)] = fivevalues[3]
+    ws['G'+ str(workingRow)] = fivevalues[4]
+    workingRow += workingRow
+
 print(introMessage)
-print(data_reader(api_call(input(tickerInput)), date_entry()))
+fivevalues = data_reader(api_call(input(tickerInput)), date_entry())
+wb = Workbook()
+ws = wb.active
+ws['A1'] = 'Date'
+ws['B1'] = 'Ticker'
+ws['C1'] = 'Open'
+ws['D1'] = 'High'
+ws['E1'] = 'Low'
+ws['F1'] = 'Close'
+ws['G1'] = 'Volume'
+rowfill(fivevalues)
+rowfill(fivevalues)
+wb.save("tester.xlsx")
